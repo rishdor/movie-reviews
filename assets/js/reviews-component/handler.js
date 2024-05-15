e5c30eb87653f16a3368858dfee6d56b6f4ee09f
Rina <doroshukmaryna@gmail.com>
2024-05-15 17:29:21 +0200

    adding reviews

--- assets/js/reviews-component/handler.js
+++ assets/js/reviews-component/handler.js
@@ -1,7 +1,11 @@
 import { fetchFromServer } from "../fetcher.js";
 import { renderReviews } from "../reviews-component/renderer.js";
 
+var currentMovie;
+
 function loadReviews(movie){
+    currentMovie = movie;
+    console.log(currentMovie)
     fetchFromServer("GET", `movies/${movie.id}/reviews`)
         .then(json => {
             const reviews = json.data;
@@ -10,7 +14,23 @@ function loadReviews(movie){
             document.getElementById('movie-details').classList.remove('hidden');
             var $title = document.querySelector('#movie-title');
             $title.innerText = movie.name;
+            $title.value = movie.id;
         })
 }
 
-export {loadReviews}
+function addReview(review){
+    fetchFromServer("POST", `movies/${review.movie_id}/reviews`, review)
+        .then(review => {
+            loadReviews(currentMovie);
+        })
+}
+
+function getReview(){
+	return {
+        movie_id: document.querySelector('#movie-title').value,
+		email: document.querySelector("#email").value,
+		message: document.querySelector("#message").value,
+        rating: parseInt(document.querySelector("#rating").value)
+	};
+}
+export {loadReviews, addReview, getReview}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 import { loadMovies } from "./movies-component/handler.js";
+import { addReview, getReview } from "./reviews-component/handler.js";
+
 
 init();
 
 function init(){
     loadMovies();
+    document.querySelector('form').addEventListener('submit', publishReview);
+}
+
+function publishReview(e){
+    e.preventDefault();
+    var review = getReview();
+    addReview(review);
 }
